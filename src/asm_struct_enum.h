4d7801a2fba051fa72bea98462b6e2947df0a055
Yves BAZIN <yvesbazin@Yvess-MacBook-Pro.local>
2024-07-04 16:01:18 +0200

    hh

--- src/asm_struct_enum.h
+++ src/asm_struct_enum.h
@@ -6,7 +6,7 @@
 #include <vector>
 #include <string>
 using namespace std;
-enum  class operandeType
+enum class operandeType
 {
   registers,
   floatregisters,
@@ -33,103 +33,95 @@ enum  class operandeType
 
 };
 
-
 enum class opCodeType
 {
-standard,
-call,
-jump,
-jump_32aligned,
-call_32aligned,
-label,
-function_declaration,
-external_var,
-external_call,
-data,
-number,
-number_label,
-data_label,
-not_known
+  standard,
+  call,
+  jump,
+  jump_32aligned,
+  call_32aligned,
+  label,
+  function_declaration,
+  external_var,
+  external_call,
+  data,
+  number,
+  number_label,
+  data_label,
+  not_known
 };
 
 enum class externalType
 {
-    function,
-    value,
+  function,
+  value,
 };
 
-typedef struct 
+typedef struct
 {
-    externalType type;
-    string name;
-    void * ptr;
-    int offset;
+  externalType type;
+  string name;
+  void *ptr;
+  int offset;
 } asm_external;
 
-
-typedef struct 
+typedef struct
 {
- string opcde;
- string operandes;
- int error;
+  string opcde;
+  string operandes;
+  int error;
 } line;
 
-
- struct error_message_struct
+struct error_message_struct
 {
   string error_message;
   int error;
-
 };
+error_message_struct asm_Error;
 
-typedef struct 
+typedef struct
 {
- error_message_struct error;
- int value;
- string label;
- uint32_t opcode;
+  error_message_struct error;
+  int value;
+  string label;
+  uint32_t opcode;
 } result_parse_operande;
 
 typedef struct result_parse_line
 {
-     error_message_struct  error;
-        uint32_t bincode;
-        uint32_t size;
-        opCodeType op;
-        string name;
-        uint32_t address;
-        bool align;
-        string debugtxt;
-        int line;
-        uint32_t (*calculateOfssetJump)(uint32_t value,uint32_t current_address,uint32_t destination_address)=NULL;
-
+  // error_message_struct  error;
+  uint32_t bincode;
+  uint32_t size;
+  opCodeType op;
+  string name;
+  uint32_t address;
+  bool align;
+  // string debugtxt;
+  // int line;
+  uint32_t (*calculateOfssetJump)(uint32_t value, uint32_t current_address, uint32_t destination_address) = NULL;
 };
-
 
 enum asmInstruction
 {
-    s8i,
-    l8ui,
-    l16ui,
-    l16si,
-    s16i,
-    l32i,
-    s32i,
-    lsi,
-    ssi,
-    add,
-    sub,
-    quou,
-    mull,
-    adds,
-    subs,
-    quous,
-    muls,
-    neg,
-    negs
-
-
-
+  s8i,
+  l8ui,
+  l16ui,
+  l16si,
+  s16i,
+  l32i,
+  s32i,
+  lsi,
+  ssi,
+  add,
+  sub,
+  quou,
+  mull,
+  adds,
+  subs,
+  quous,
+  muls,
+  neg,
+  negs
 
 };
 
@@ -145,61 +137,61 @@ string asmInstructionsName[] =
         "lsi",
         "ssi",
         "add",
-    "sub",
-    "quou",
-    "mull",
-    "add.s",
-    "sub.s",
-    "quou",
-    "mul.s",
-    "neg",
-    "neg.s"
-        
+        "sub",
+        "quou",
+        "mull",
+        "add.s",
+        "sub.s",
+        "quou",
+        "mul.s",
+        "neg",
+        "neg.s"
+
 };
 
-typedef struct {
+typedef struct
+{
   string name;
   uint32_t address;
-} globalcall; 
+} globalcall;
 
 typedef struct
 {
   error_message_struct error;
   vector<globalcall> functions;
-  uint32_t *start_program=NULL;
-  uint8_t *data=NULL;
+  uint32_t *start_program = NULL;
+  uint8_t *data = NULL;
   uint32_t links;
 
 } executable;
 
-
 operandeType op_mov_n[2] = {operandeType::registers, operandeType::registers};
-operandeType *op_mov=op_mov_n; //[3] = {operandeType::registers, operandeType::registers};
+operandeType *op_mov = op_mov_n; //[3] = {operandeType::registers, operandeType::registers};
 operandeType op_l8ui[3] = {operandeType::registers, operandeType::registers, operandeType::l0_255};
-operandeType *op_s8i=op_l8ui;  //[3] = {operandeType::registers, operandeType::registers, operandeType::l0_255};
+operandeType *op_s8i = op_l8ui; //[3] = {operandeType::registers, operandeType::registers, operandeType::l0_255};
 operandeType op_l16si[3] = {operandeType::registers, operandeType::registers, operandeType::l0_510};
-operandeType *op_l16ui= op_l16si;   //[3] = {operandeType::registers, operandeType::registers, operandeType::l0_510};
-operandeType *op_s16i =op_l16si;    //[3] = {operandeType::registers, operandeType::registers, operandeType::l0_510};
+operandeType *op_l16ui = op_l16si; //[3] = {operandeType::registers, operandeType::registers, operandeType::l0_510};
+operandeType *op_s16i = op_l16si;  //[3] = {operandeType::registers, operandeType::registers, operandeType::l0_510};
 operandeType op_l32i_n[3] = {operandeType::registers, operandeType::registers, operandeType::l0_60};
 operandeType op_s32i[3] = {operandeType::registers, operandeType::registers, operandeType::l0_1020};
-operandeType *op_s32i_n=op_l32i_n;    //[3] = {operandeType::registers, operandeType::registers, operandeType::l0_60};
-operandeType *op_l32i =op_s32i;   // [3] = {operandeType::registers, operandeType::registers, operandeType::l0_1020};
+operandeType *op_s32i_n = op_l32i_n; //[3] = {operandeType::registers, operandeType::registers, operandeType::l0_60};
+operandeType *op_l32i = op_s32i;     // [3] = {operandeType::registers, operandeType::registers, operandeType::l0_1020};
 operandeType op_lsi[3] = {operandeType::floatregisters, operandeType::registers, operandeType::l0_1020};
-operandeType *op_ssi=op_lsi;          //[3] = {operandeType::floatregisters, operandeType::registers, operandeType::l0_1020};
+operandeType *op_ssi = op_lsi; //[3] = {operandeType::floatregisters, operandeType::registers, operandeType::l0_1020};
 operandeType op_rfr[2] = {operandeType::registers, operandeType::floatregisters};
 operandeType op_wfr[2] = {operandeType::floatregisters, operandeType::registers};
-operandeType op_add[3] = {operandeType::registers, operandeType::registers,  operandeType::registers};
+operandeType op_add[3] = {operandeType::registers, operandeType::registers, operandeType::registers};
 operandeType *op_quou = op_add;
 operandeType *op_sub = op_add;
 operandeType *op_mull = op_add;
-operandeType op_adds[3] = {operandeType::floatregisters, operandeType::floatregisters,  operandeType::floatregisters};
+operandeType op_adds[3] = {operandeType::floatregisters, operandeType::floatregisters, operandeType::floatregisters};
 operandeType *op_quous = op_adds;
 operandeType *op_subs = op_adds;
 operandeType *op_muls = op_adds;
 operandeType *op_neg = op_mov;
 operandeType op_nexp01s[2] = {operandeType::floatregisters, operandeType::floatregisters};
 
-operandeType *op_negs=  op_nexp01s; 
+operandeType *op_negs = op_nexp01s;
 operandeType *asmInstructionOperandes[] =
     {
         op_s8i,
@@ -211,10 +203,10 @@ operandeType *asmInstructionOperandes[] =
         op_s32i,
         op_lsi,
         op_ssi,
-        op_add, //add
-        op_sub, //sub
-        op_quou, //quou
-        op_mull, //mull
+        op_add,  // add
+        op_sub,  // sub
+        op_quou, // quou
+        op_mull, // mull
         op_adds,
         op_subs,
         op_quou,
@@ -222,38 +214,33 @@ operandeType *asmInstructionOperandes[] =
         op_neg,
         op_negs,
 };
-operandeType * __l;
- operandeType __op;
+operandeType *__l;
+operandeType __op;
 
-
-string getRegType(asmInstruction instr,int pos)
+string getRegType(asmInstruction instr, int pos)
 {
-  if(instr>=19)
+  if (instr >= 19)
   {
     printf("to hight\r\n");
     return " ";
   }
 
-  __l=asmInstructionOperandes[instr];
-  __op=__l[pos];
-  
-    if(__op==operandeType::registers)
-    {
+  __l = asmInstructionOperandes[instr];
+  __op = __l[pos];
+
+  if (__op == operandeType::registers)
+  {
     return "a";
-    }
-    else if(__op==operandeType::floatregisters)
-    {
-     
+  }
+  else if (__op == operandeType::floatregisters)
+  {
+
     return "f";
-    }
-    else{
-      return "unknown";
-    }
-
-    
-  
-
+  }
+  else
+  {
+    return "unknown";
+  }
 }
-
 
 #endif
