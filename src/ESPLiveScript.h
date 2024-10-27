5928b9dde7e88822d859ed28e0f621c19dfb2e03
Yves BAZIN <yvesbazin@Yvess-MacBook-Pro.local>
2024-10-27 18:40:42 +0100

    runtime

--- src/ESPLiveScript.h
+++ src/ESPLiveScript.h
@@ -1989,7 +1989,7 @@ current_node->addChild(NodeToken(statementNode));
                 current_node=current_node->parent;
                 current_node=current_node->parent;
 
-                current_node=current_node->parent;
+                //current_node=current_node->parent;
                 change_type.pop_back();
                 return;
             }
