b529755688cad7c0873a73bad30447ab67759523
Yves BAZIN <yvesbazin@Yvess-MacBook-Pro.local>
2025-05-22 09:46:10 +0200

    adding json example

---
+++ examples/jsonexample/jsonexample.ino
@@ -0,0 +1,47 @@
+#include "ESPLiveScript.h"
+
+string script = R"EOF(
+json dp.value as int g2;
+json dp.test as int g;
+json dp2 as float x;
+int fib(int h)
+{
+   if(h<=1)
+   return h;
+   return fib(h-1)+fib(h-2);
+}
+
+void main()
+{  
+
+  int d=x*5;
+  printfln("%d",d);
+   printfln("fib of %d is %d",g2,fib(g2));
+   printfln("fib of %d is %ld",g,fib(g));
+   
+}
+)EOF";
+
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    Parser p;
+    Executable exec = p.parseScript(&script);
+    if (exec.isExeExists())
+    {
+
+        exec.execute("main", "{\"dp\":{\"value\":6,\"test\":12},\"dp2\":4.5}");
+         exec.execute("main", "{\"dp\":{\"value\":10,\"test\":20}}");
+    }
+    else
+    {
+        printf("%s\n", exec.error.error_message.c_str());
+    }
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+}
